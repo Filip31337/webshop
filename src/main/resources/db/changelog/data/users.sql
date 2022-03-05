068bde488be19b69ed4aca31211431dd6e0cb39d
filip.brajnovic <filip.brajnovic@gmail.com>
2022-03-05 20:20:31 +0100

    added ddl

---
+++ src/main/resources/db/changelog/data/users.sql
@@ -0,0 +1,8 @@
+INSERT INTO USER(id, username, password, email, first_name, last_name, phone_number, enabled)
+VALUES (SEQ_USER.NEXTVAL, 'admin', 'admin', 'admin@admin.com', 'Robert', 'De Niro', '0038598777666', true);
+
+INSERT INTO USER(id, username, password, email, first_name, last_name, phone_number, enabled)
+VALUES (SEQ_USER.NEXTVAL, 'tom', 'pass1', 'tom@cruise.com', 'Tom', 'Cruise', '0038598777666', true);
+
+INSERT INTO USER(id, username, password, email, first_name, last_name, phone_number, enabled)
+VALUES (SEQ_USER.NEXTVAL, 'brad', 'pass1', 'tom@cruise.com', 'Brad', 'Pitt', '0038598777666', false);
