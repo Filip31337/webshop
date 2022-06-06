7f79c457b3cbb5fc05ebab81a166cbc85d5cc542
filip.brajnovic <filip.brajnovic@gmail.com>
2022-06-06 18:11:09 +0200

    handled no results in service layer

--- src/main/java/com/brajnovic/webshop/controller/UserController.java
+++ src/main/java/com/brajnovic/webshop/controller/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+
 @RestController
 @RequestMapping(UrlConstants.APP_CONTEXT_ROOT)
 public class UserController {
@@ -23,31 +25,56 @@ public class UserController {
     @GetMapping(UrlConstants.GET_USER_BY_ID +"/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable Long userId) {
         if (userId == null) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.internalServerError().build();
         }
-        User response = userService.getUserById(userId);
+        User user;
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        try {
+            user = userService.getUserById(userId);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.noContent().header("errorMessage", e.getMessage()).build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().header("errorMessage", e.getMessage()).build();
+        }
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @GetMapping(UrlConstants.GET_USER_BY_USER_NAME + "/{userName}")
     public ResponseEntity<User> getUserByName(@PathVariable String userName) {
         if (userName == null) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.internalServerError().build();
         }
-        User response = userService.getUserByName(userName);
+        User user;
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        try {
+            user = userService.getUserByName(userName);
+
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.noContent().header("errorMessage", e.getMessage()).build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().header("errorMessage", e.getMessage()).build();
+        }
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping(UrlConstants.GET_USER)
     public ResponseEntity<User> getUser(@RequestBody GetUserCommand getUserCommand) {
         if (getUserCommand == null) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.internalServerError().build();
         }
-        User response = userService.getUser(getUserCommand);
+        User user;
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        try {
+            user = userService.getUser(getUserCommand);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.noContent().header("errorMessage", e.getMessage()).build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().header("errorMessage", e.getMessage()).build();
+        }
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/brajnovic/webshop/service/impl/UserServiceImpl.java
+++ src/main/java/com/brajnovic/webshop/service/impl/UserServiceImpl.java
@@ -4,9 +4,10 @@ import com.brajnovic.webshop.command.GetUserCommand;
 import com.brajnovic.webshop.entity.UserEntity;
 import com.brajnovic.webshop.model.User;
 import com.brajnovic.webshop.repository.UserRepository;
-import liquibase.pro.packaged.U;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,41 +20,42 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUserById(Long personId) {
+    public User getUserById(Long personId) throws EntityNotFoundException, IllegalArgumentException {
         if (personId == null) {
             throw new IllegalArgumentException("Prazan je personId argument");
         }
 
-        UserEntity userEntity = userRepository.findById(personId).orElse(null);
+        UserEntity userEntity = userRepository.findById(personId).orElseThrow(() -> new EntityNotFoundException("No user found by id: " + personId));
 
         return UserEntity.convertToUser(userEntity);
     }
 
     @Override
-    public User getUserByName(String userName) {
+    public User getUserByName(String userName) throws EntityNotFoundException, IllegalArgumentException {
         if (userName == null || userName.isEmpty()) {
             throw new IllegalArgumentException("Prazan je userName argument");
         }
+        UserEntity userEntity;
 
-        UserEntity userEntity = userRepository.findByUsername(userName).orElse(null);
+        userEntity = userRepository.findByUsername(userName).orElseThrow(() -> new EntityNotFoundException("No user found by name: " + userName));
+
 
         return UserEntity.convertToUser(userEntity);
     }
 
     @Override
-    public User getUser(GetUserCommand getUserCommand) {
+    public User getUser(GetUserCommand getUserCommand) throws EntityNotFoundException, IllegalArgumentException {
         if (getUserCommand == null) {
             throw new IllegalArgumentException("Nedostaje command getUserCommand.");
         }
-        UserEntity userEntity = new UserEntity();
+        UserEntity userEntity;
 
-         if (userRepository.existsById(getUserCommand.getPersonId())) {
-             userEntity = userRepository.findById(getUserCommand.getPersonId()).orElse(null);
-         } else {
-             userEntity = userRepository.findByUsername(getUserCommand.getPersonName()).orElse(null);
-         }
+        userEntity = userRepository.findById(getUserCommand.getPersonId())
+                .orElseGet(() -> userRepository.findByUsername(getUserCommand.getPersonName())
+                .orElseThrow(() -> new EntityNotFoundException("No user found by name: " + getUserCommand.getPersonName() + " or id: " + getUserCommand.getPersonId().toString()))
+        );
 
-         return UserEntity.convertToUser(userEntity);
+        return UserEntity.convertToUser(userEntity);
     }
 
 }
